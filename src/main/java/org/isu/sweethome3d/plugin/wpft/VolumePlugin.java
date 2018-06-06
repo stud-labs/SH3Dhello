104a23ebd5060c60e2d26bf33ce1b8cf6a61e2cd
eanischenko <Katya.A96@yandex.ru>
2018-06-06 16:50:22 +0800

    Added new database entities.

--- src/main/java/org/isu/sweethome3d/plugin/wpft/VolumePlugin.java
+++ src/main/java/org/isu/sweethome3d/plugin/wpft/VolumePlugin.java
@@ -2,10 +2,15 @@ package org.isu.sweethome3d.plugin.wpft;
 
 import com.eteks.sweethome3d.plugin.Plugin;
 import com.eteks.sweethome3d.plugin.PluginAction;
+import org.hibernate.Session;
 
 public class VolumePlugin extends Plugin {
-    @Override
-    public PluginAction[] getActions() {
-        return new PluginAction [] {new VolumeAction(getHome())};
+    public  VolumePlugin(){
+        super();
+        if (session = null)
     }
+    @Override
+   public PluginAction[] getActions() {return new PluginAction [] {new VolumeAction(getHome())};    }
+
+   public static Session session = null;
 }
